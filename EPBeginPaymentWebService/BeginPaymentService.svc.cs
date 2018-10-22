562c4ade1a298fd6d4e51a442d7e6d9b71fd1324
Jesús Salvador Carrillo Gonzalez <co_jcarillog@axtel.com.mx>
2018-10-22 15:55:11 -0500

    Se quita la libreria de Nlog , se implementa serilog.

--- EPBeginPaymentWebService/BeginPaymentService.svc.cs
+++ EPBeginPaymentWebService/BeginPaymentService.svc.cs
@@ -8,6 +8,10 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
 using Dapper;
+using Serilog;
+using Serilog.Formatting.Compact;
+using Serilog.Formatting.Json;
+using EPBeginPaymentWebService.Utilities;
 
 namespace EPBeginPaymentWebService
 {
@@ -19,25 +23,63 @@ namespace EPBeginPaymentWebService
 
         public void CreateBeginPayment(BeginPayment bpo)
         {
+            var logger = new LoggerConfiguration()
+                         .MinimumLevel.Debug()
+                         .WriteTo.RollingFile(new CompactJsonFormatter(), @"E:\LOG\EnterprisePaymentLog.json")
+                         .CreateLogger();
+
+            
             try
             {
+           
+
+                int id;
                 using (IDbConnection conn = Connection)
                 {
                     var parameters = new DynamicParameters();
 
-                    parameters.Add("@BILLING_ACCOUNT", bpo.BillingAccount);
-                    parameters.Add("@SERVICE_REQUEST", bpo.ServiceRequest);
-                    parameters.Add("@PAYMENT_REFERENCE", bpo.PaymentReference);
-                    parameters.Add("@CREATE_TOKEN", bpo.CreateToken);
-                    parameters.Add("@BEGIN_PAYMENT_ID", dbType: DbType.Int32, direction: ParameterDirection.Output);
+                    StaticBeginEnterpricePayment.BillingAccount = bpo.BillingAccount;
+                    StaticBeginEnterpricePayment.ServiceRequest = bpo.ServiceRequest;
+                    StaticBeginEnterpricePayment.PaymentReference = bpo.PaymentReference;
+                    StaticBeginEnterpricePayment.CreateToken = bpo.CreateToken;
+
+
+                    parameters.Add(StaticBeginEnterpricePayment.BILLING_ACCOUNT, bpo.BillingAccount);
+                    parameters.Add(StaticBeginEnterpricePayment.SERVICE_REQUEST, bpo.ServiceRequest);
+                    parameters.Add(StaticBeginEnterpricePayment.PAYMENT_REFERENCE, bpo.PaymentReference);
+                    parameters.Add(StaticBeginEnterpricePayment.CREATE_TOKEN, bpo.CreateToken);
+                    parameters.Add(StaticBeginEnterpricePayment.BEGIN_PAYMENT_ID, 
+                                    dbType: DbType.Int32, 
+                                    direction: ParameterDirection.Output);
 
                     conn.Open();
-                    conn.QueryAsync("SP_CREATE_BEGIN_ENTERPRISE_PAYMENT", parameters, commandType: CommandType.StoredProcedure);
+
+                    logger.Information(StaticBeginEnterpricePayment.LogTemplateBeforeInsert, 
+                                        bpo.BillingAccount,
+                                        bpo.ServiceRequest,
+                                        bpo.PaymentReference,
+                                        bpo.CreateToken,
+                                        StaticBeginEnterpricePayment.PaymentStage,
+                                        StaticBeginEnterpricePayment.ComunicationStep);
+
+                    conn.QueryAsync(StaticBeginEnterpricePayment.SP_CREATE_BEGIN_ENTERPRISE_PAYMENT, 
+                                      parameters, 
+                                      commandType: CommandType.StoredProcedure);
+
+                    id = parameters.Get<int>(StaticBeginEnterpricePayment.BEGIN_PAYMENT_ID_OUTPUT_SEARCH);
+
+                    StaticBeginEnterpricePayment.BeginPaymentGeneratedId = id;
+
+                    logger.Information( StaticBeginEnterpricePayment.LogTemplateAfterInsert,
+                                        id, 
+                                        bpo.ServiceRequest,
+                                        bpo.BillingAccount);
+
                 }
             }
-            catch
+            catch(Exception ex)
             {
-
+                logger.Error(ex, $"Failed Operation for Service Request:{bpo.ServiceRequest} , Billing Account: {bpo.BillingAccount} and Payment Reference: {bpo.PaymentReference}  ");
             }
         }
     }
---
+++ EPBeginPaymentWebService/Utilities/StaticBeginEnterpricePayment.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EPBeginPaymentWebService.Utilities
+{
+    public static class StaticBeginEnterpricePayment
+    {
+
+        public static string BillingAccount { get; set; }
+        public static string ServiceRequest { get; set; }
+        public static string PaymentReference { get; set; }
+        public static string CreateToken { get; set; }
+        public static int BeginPaymentGeneratedId { get; set; }
+
+
+
+        public static string BILLING_ACCOUNT
+        {
+            get
+            {
+                return "@BILLING_ACCOUNT";
+            }
+        }
+
+        public static string SERVICE_REQUEST
+        {
+            get
+            {
+                return "@SERVICE_REQUEST";
+            }
+        }
+
+        public static string PAYMENT_REFERENCE
+        {
+            get
+            {
+                return "@PAYMENT_REFERENCE";
+            }
+        }
+
+        public static string CREATE_TOKEN
+        {
+            get
+            {
+                return "@CREATE_TOKEN";
+            }
+        }
+
+        public static string BEGIN_PAYMENT_ID
+        {
+            get
+            {
+                return "@BEGIN_PAYMENT_ID";
+            }
+        }
+
+        public static string BEGIN_PAYMENT_ID_OUTPUT_SEARCH
+        {
+            get
+            {
+                return "BEGIN_PAYMENT_ID";
+            }
+        }
+
+
+        public static string LogTemplateBeforeInsert
+        {
+
+            get
+            {
+                return "Before Insert Begin Payment with the following data:" +
+                                        "BillingAccount: {BillingAccount} " +
+                                        "ServiceRequest: {ServiceRequest} " +
+                                        "PaymentReference:{PaymentReference} " +
+                                        "CreateToken:{CreateToken} " +
+                                        "PaymentStage:{LogPaymentStage} " +
+                                        "ComunicationStep:{LogComunicationStep} ";
+
+            }
+        }
+
+        public static string PaymentStage
+        {
+            get
+            {
+                return "BeginPayment";
+            }
+        }
+
+        public static string ComunicationStep
+        {
+            get
+            {
+                return "FROM SIEBEL TO .NET";
+            }
+        }
+
+        public static string SP_CREATE_BEGIN_ENTERPRISE_PAYMENT
+        {
+            get
+            {
+                return "SP_CREATE_BEGIN_ENTERPRISE_PAYMENT";
+            }
+        }
+
+        public static string LogTemplateAfterInsert
+        {
+            get
+            {
+
+                return "After Insert Begin Payment with the following data:" +
+                        "Generated Id for Begin Payment {BeginPaymentGeneratedId}" +
+                        "from the ServiceRequest: {ServiceRequest}" +
+                        "and BillingAccount: {BillingAccount}";
+            }
+        }
+    }
+}
